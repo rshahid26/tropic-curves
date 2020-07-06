8a782ea5e9639b8a52042332594a2a5fc6faec2b
Connor Meredith <connor.meredith0913@gmail.com>
2020-07-06 17:11:49 -0600

    Add marking property

--- Tropical2020/basic_families/Leg.py
+++ Tropical2020/basic_families/Leg.py
@@ -1,9 +1,10 @@
 class Leg(object):
     # name_ should be a string identifier - only unique if the user is careful (or lucky) to make it so
     # root_ should be a vertex
-    def __init__(self, name_, root_):
+    def __init__(self, name_, root_, marking_ = 0):
         self.name = name_
         self.root = root_
+        self.marking = marking_
 
     # The set of vertices is a read only property computed upon access
     @property
